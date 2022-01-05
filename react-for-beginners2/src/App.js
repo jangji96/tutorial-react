0a7eb9e829a0002fad66ff328b9340914ddd66f1
jangji96 <xxwlgns@naver.com>
2022-01-05 17:20:43 +0900

    EFFECTS #6.0 Introduction

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("call an api");
   return (
     <div>
-      <h1 className={styles.title}>Welcom back!</h1>
-      <Button text={"continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
