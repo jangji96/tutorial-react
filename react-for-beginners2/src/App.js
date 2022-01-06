6088e1b6323e1f6ccc280e7aca498ec28a4ffeab
jangji96 <xxwlgns@naver.com>
2022-01-06 09:36:47 +0900

    #6.2 Deps

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -2,13 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyowrd' chabges.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' chabges.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter chabge.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="test"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
