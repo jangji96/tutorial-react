0b6c5dcea06af87cdbc48b55ede0a46cc56e6eda
jangji96 <xxwlgns@naver.com>
2022-01-20 15:58:09 +0900

    #7.6 Parameters

--- movie-app/src/components/movie.js
+++ movie-app/src/components/movie.js
@@ -1,27 +1,25 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const Movie = ({ coverImg, title, summary, genres }) => {
+const Movie = ({ id, coverImg, title, summary, genres }) => {
   return (
     <div>
       <img src={coverImg} />
       <h2>
-        <Link to={"/movie"}>{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
-        {" "}
-        <li>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </li>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
       </ul>
     </div>
   );
 };
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
